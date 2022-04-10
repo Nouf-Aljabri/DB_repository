697ade20883528bb1c69a80829b1b6ffbbf91ee0
Nouf Aljabri <n-aj1@hotmail.com>
2022-04-10 12:26:18 +0300

    commit1

---
+++ Programmability/Procedures/dbo.Customer_Add.sql
@@ -0,0 +1,18 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+
+create procedure [dbo].[Customer_Add]  
+ @customer_Name  nvarchar(250),
+ @national_ID nvarchar(50),
+ @vehicle nvarchar(100),
+ @insurance_price nvarchar(100),  
+ @quotation_id nvarchar(100),
+ @policy_number nvarchar(100)
+  
+ AS  
+ BEGIN  
+ Insert into tbl_Customer(customer_Name,national_ID,vehicle,insurance_price,quotation_id,policy_number)  
+ values(@customer_Name,@national_ID,@vehicle,@insurance_price,@quotation_id,@policy_number)   
+ END
+
+GO
---
+++ Tables/dbo.tbl_Customer.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tbl_Customer] (
+  [id] [int] IDENTITY,
+  [customer_Name] [nvarchar](250) NULL,
+  [national_ID] [nvarchar](50) NULL,
+  [vehicle] [nvarchar](100) NULL,
+  [insurance_price] [nvarchar](100) NULL,
+  [quotation_id] [nvarchar](100) NULL,
+  [policy_number] [nvarchar](100) NULL,
+  PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO
